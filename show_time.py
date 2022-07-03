412f7601fcf2e1aab424fc925cc7966ebc4e727b
Srushti15 <panchal.sr@northeastern.edu>
2022-07-03 15:49:31 -0400

    Code for date & time

--- show_time.py
+++ show_time.py
@@ -1,4 +1,4 @@
 import datetime
 now = datetime.datetime.now()
-print ("Current time : ")
-print (now.strftime("%H:%M:%S"))
+print ("Current Date & Time : ")
+print (now.strftime("%d-%m-%Y %H:%M:%S"))
